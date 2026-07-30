20495c4a16defea75c57f6014a1304effb26ee8c
zhujiani111 <zhujiani111@users.noreply.github.com>
2026-07-30 16:38:05 +0800

    chore: bump service worker cache to v12

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'xiaofen-workbench-v11';
+const CACHE_NAME = 'xiaofen-workbench-v12';
 const urlsToCache = ['./index.html', './manifest.json'];
 
 self.addEventListener('install', event => {
